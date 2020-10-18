9385128547e6b054c2793f45729a32f520805fb7
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-10-18 18:58:31 +0300

    index.js: Render heading elements into root

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as Heading from './components/heading.js';
+
+const root = document.getElementById("root");
+
+root.appendChild(Heading.heading("Normal Heading"));
+
+Heading.asyncHeading("Async Heading").then((asyncHeading) => {
+    root.appendChild(asyncHeading);
+});
