eb75a4e614807c2b36138da92cfcf7a2eb6a7382
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-10-19 11:13:26 +0300

    index.js: Append image under root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+// Styles
 import './style.scss';
+
+// Assets
+import logo from './assets/infinite_workout.png';
+
+// Components
 import * as Heading from './components/heading.js';
 import * as Paragraph from './components/paragraph';
+import * as Image from './components/image';
 
 const root = document.getElementById("root");
 
@@ -11,3 +18,5 @@ Heading.asyncHeading("Async Heading").then((asyncHeading) => {
 });
 
 root.appendChild(Paragraph.paragraph("Paragraph"));
+
+root.appendChild(Image.image(logo));
